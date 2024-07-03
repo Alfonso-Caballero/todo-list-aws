aaa347887eeb710d40cf794eb180a5e333774a66
Alfonso <135018165+Alfonso-Caballero@users.noreply.github.com>
2024-07-03 11:26:55 +0200

    Update todoApiTest.py

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -163,6 +163,7 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Update TODO')
+    @pytest.mark.read_only
     def test_api_deletetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Delete TODO')
